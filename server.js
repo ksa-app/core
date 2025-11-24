542efe8cf57a9d7ba0d3602d881b4063acfe4a2e
riajul-swe <riajul.swe@gmail.com>
2025-11-24 15:58:53 +0600

    ok

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import fs from "fs";
+
+const app = express();
+app.use(express.json());
+
+// GET all candidates
+app.get("/candidates", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("candidates.json"));
+  res.json(data);
+});
+
+// ADD new candidate
+app.post("/candidates", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("candidates.json"));
+  const newCandidate = req.body;
+
+  data.push(newCandidate);
+  fs.writeFileSync("candidates.json", JSON.stringify(data, null, 2));
+
+  res.json({ success: true, message: "Candidate Added", data: newCandidate });
+});
+
+// Root path
+app.get("/", (req, res) => {
+  res.send("Simple Candidate API Running!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
