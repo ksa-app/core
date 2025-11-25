386c200ec2bd0f7240d9fe481b618b078280c094
riajul-swe <sevenstar2202@gmail.com>
2025-11-25 14:01:54 +0600

    ok

--- routes/candidates.js
+++ routes/candidates.js
@@ -3,23 +3,23 @@ import Candidate from "../models/Candidate.js";
 
 const router = express.Router();
 
-router.post('/',async (req,res)=>{
+// CREATE Canidate
+router.post('/',async(req,res)=>{
     try{
-        const candiate = await Candidate.create(req.body);
-        res.status(201).json(candiate);
+        const candidate = await Candidate.create(req.body);
+        res.status(201).json(candidate);
     }catch(err){
-        res.status(404).json({error:err.message});
+        res.status(400).json({error:err.message});
     }
 });
+//REAZ Candidate
+
 router.get('/',async(req,res)=>{
     try{
         const candidates = await Candidate.find();
-        res.json(candidates);
+        res.json(candidates)
     }catch(err){
-        res.status(500).json({error:err.message})
+        res.status(500).json({error:err.message});
     }
 });
-// router.get();
-// router.put();
-// router.delete();
 export default router;
--- server.js
+++ server.js
@@ -3,16 +3,24 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import router from "./routes/candidates.js";
+
 dotenv.config();
 connectDB();
+
+//Midlewares
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Routes
 app.use("/api/canidates",router);
+
+//TEST Routes
 app.get('/',(req,res)=>{
   res.send("candiate appi ");
 })
+
+//Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
   console.log(`server running on port ${PORT}`);
