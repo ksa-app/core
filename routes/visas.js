9737fb798b38ee08d218bb1ef7bab844ad2e0112
riajul-swe <sevenstar2202@gmail.com>
2025-11-26 20:15:45 +0600

    ok

--- routes/visas.js
+++ routes/visas.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import Visa from '../models/Visa.js';
-
+import Candidate from '../models/Candidate.js';
 const router = express.Router();
 
 router.post("/", async(req,res)=>{
     try{
         const visa = await Visa.create(req.body);
         res.json(visa);
+        await Candidate.findByIdAndUpdate(
+            req.body.candidate,{
+                status:"VISA ISSUED",
+                visaId:visa._id
+            }
+        );
     }catch(error){
         res.status(400).json({messege:error.messege});
     }
