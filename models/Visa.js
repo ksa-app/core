09ee1408b1eef9a372885e2153d3900907d0a1a9
riajul-swe <sevenstar2202@gmail.com>
2025-11-26 20:02:48 +0600

    ok

--- models/Visa.js
+++ models/Visa.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 const visaSchema = new mongoose.Schema({
-    candidate:{type:mongoose.Schema.Types.ObjectId,ref:"Candidate",required:true},
+    candidate:{type:mongoose.Schema.Types.ObjectId,ref:"Candidate",required:true,unique:true},
     issueDate:{type:Date},
     expiryDate:{type:Date},
     status:{type:String, enum:["ISSUDE","SOLD","EXPIRED","CANCEL"],default:"ISSUDE"},
+    visaCopy:{type:String},
+    visaType:{type:String},
+    visaFrom:{type:String}
 },{timestamps:true});
 
 
